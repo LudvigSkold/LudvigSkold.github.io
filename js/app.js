ff2f22a8a10abb6c6480c8496ac982dcc28ba2d6
LudvigSkold <ludvig.skold@gmail.com>
2022-03-22 10:53:26 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            isHidden: true,
+            isHidden2: true
+        },
+        mounted() {
+        },
+        methods: {
+            
+        },
+        computed: {
+            
+        }
+    })
